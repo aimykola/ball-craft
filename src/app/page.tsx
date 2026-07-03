d8a46d1ac5348fed0422c09300694a3045c552ba
aimykola <aimykola15@gmail.com>
2026-07-03 09:12:42 +0300

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+import Header from '@/components/Header'
+import Catalog from '@/components/Catalog'
+import CartDrawer from '@/components/cart/CartDrawer'
+import { Hero, About, Reviews, Footer } from '@/components/Sections'
+import Contacts from '@/components/ContactsSection'
+import FloatingContact from '@/components/FloatingContact'
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <About />
+        <Catalog />
+        <Reviews />
+        <Contacts />
+      </main>
+      <Footer />
+      <CartDrawer />
+      <FloatingContact />
+    </>
+  )
+}
