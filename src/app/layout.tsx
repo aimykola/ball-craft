1cdf8eeaefa951595d503c898648d59252d3220d
aimykola <aimykola15@gmail.com>
2026-07-12 18:44:03 +0300

    Wrap app with LanguageProvider (i18n)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Forum, Cormorant } from 'next/font/google'
 import './globals.css'
 import { CartProvider } from '@/components/cart/CartContext'
 import { FavoritesProvider } from '@/components/favorites/FavoritesContext'
+import { LanguageProvider } from '@/components/LanguageContext'
 
 const forum = Forum({ subsets: ['latin', 'cyrillic'], weight: '400', variable: '--font-sans' })
 const cormorant = Cormorant({ subsets: ['latin', 'cyrillic'], weight: ['400', '500', '600'], variable: '--font-serif' })
@@ -16,11 +17,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="uk" className={`${forum.variable} ${cormorant.variable}`}>
       <body>
+        <LanguageProvider>
         <CartProvider>
           <FavoritesProvider>
             {children}
           </FavoritesProvider>
         </CartProvider>
+        </LanguageProvider>
       </body>
     </html>
   )
